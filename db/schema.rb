3290cb8b784c295915012822bd014cda41cf9b15
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2020-03-12 22:19:00 -0600

    [ADD] new columns on schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_200601) do
+ActiveRecord::Schema.define(version: 2020_03_12_164425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,11 +45,9 @@ ActiveRecord::Schema.define(version: 2020_03_11_200601) do
   create_table "selections", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
     t.bigint "meal_id"
     t.string "month"
     t.index ["meal_id"], name: "index_selections_on_meal_id"
-    t.index ["user_id"], name: "index_selections_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
