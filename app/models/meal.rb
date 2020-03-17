879e1922129f08920107957b04caf3a8426e9a30
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2020-03-17 12:19:12 -0600

    [ADD] validations to the images

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,6 +2,6 @@ class Meal < ApplicationRecord
   has_one_attached :img
   has_one :selection
   validates :name, presence: true
-  validates :img, presence: true
+  validates :img, presence: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'] 
   validates_associated :selection
 end
