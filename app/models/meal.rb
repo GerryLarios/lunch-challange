1df4a8c71c68a3fbfc751318ef8f4622f44b0bac
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2020-03-19 19:18:44 -0600

    [REF] validations on meals

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -4,5 +4,11 @@ class Meal < ApplicationRecord
   has_and_belongs_to_many :selections
 
   validates :name, presence: true
+  validates :user, presence: true
   validates :img, presence: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+  validate :check_user_admin
+
+  def check_user_admin
+    errors.add(:user, :invalid, message: 'Only admins can create meals') unless self.user&.admin?
+  end
 end
