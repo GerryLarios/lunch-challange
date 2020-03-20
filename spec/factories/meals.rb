945c16a9e82a2dd7e00d6ceb1bc767eea12faaf3
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2020-03-19 19:17:32 -0600

    [REF] factories for tests

--- spec/factories/meals.rb
+++ spec/factories/meals.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :meal do
     name { 'Tacos' }
-    user
+    association :user, factory: :user, admin: true
     after(:build) do |meal|
       meal.img.attach(io: File.open(Rails.root.join('food.png')), filename: 'food.png', content_type: 'image/png')
     end
--- spec/factories/selections.rb
+++ spec/factories/selections.rb
@@ -2,6 +2,10 @@ FactoryBot.define do
   factory :selection do
     month { 'march' }
     association :user, factory: :user, admin: true
+    after(:build) do |selection|
+      selection.meals << create(:meal, user: selection.user)
+    end
+
     factory :selection_with_meals do
       transient { meals_count{ 5 } }
       after(:build) do |selection, evaluator|
