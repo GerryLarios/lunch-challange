3b1a62a70b970f357b1577aff303d1821ad97a11
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2020-03-22 17:10:18 -0600

    [ADD] mails to the admin when a user selects their food

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :meals
   has_many :selections
   has_secure_password
-
   validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  
+  scope :admin_emails, -> { self.where(admin: true).pluck(:email) }
 end
