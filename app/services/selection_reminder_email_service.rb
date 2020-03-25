d17234ed672bc402e1a828c6ec27653508a28ac2
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2020-03-24 22:53:30 -0600

    [ADD] redis to sidekiq

--- app/services/selection_reminder_email_service.rb
+++ app/services/selection_reminder_email_service.rb
@@ -1,7 +1,7 @@
 class SelectionReminderEmailService
   def send
     User.find_each do |user|
-      mail = UserMailer.with(user: user).selection_reminder.deliver_later if !has_next_selection?(user)
+      mail = UserMailer.with(user: user).selection_reminder.deliver if !has_next_selection?(user)
       puts mail.to if mail
     end
   end
